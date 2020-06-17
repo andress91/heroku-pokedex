345f7fc0ad1d47c6a7b093bdee48b25db2cce95a
Humberto <humberto.gonzalez.a@mail.pucv.cl>
2020-06-17 17:04:02 -0400

    fix RestTemplate

--- src/main/java/com/humberto/springboot/pokedex/app/AppConfig.java
+++ src/main/java/com/humberto/springboot/pokedex/app/AppConfig.java
@@ -1,5 +1,6 @@
 package com.humberto.springboot.pokedex.app;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,8 +9,8 @@ import org.springframework.web.client.RestTemplate;
 public class AppConfig {
 
 	@Bean // Para realizar la consulta a la API
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
 	}
 
 }

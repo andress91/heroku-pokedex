b69329674ac3a6d86f9762fba0ea118bb29d1af4
Humberto <humberto.gonzalez.a@mail.pucv.cl>
2020-06-17 15:52:53 -0400

    add project empty

---
+++ src/main/java/com/humberto/springboot/pokedex/app/SpringBootPokedexV2Application.java
@@ -0,0 +1,13 @@
+package com.humberto.springboot.pokedex.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPokedexV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPokedexV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/humberto/springboot/pokedex/app/SpringBootPokedexV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humberto.springboot.pokedex.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPokedexV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
